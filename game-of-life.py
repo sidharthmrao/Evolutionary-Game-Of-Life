af117546c620a47234127f40dfc837a303425d28
Sidharth Rao <sidharthmrao@gmail.com>
2021-05-28 03:33:14 -0400

    Added better display functionality along with support for Linux and Windows.

--- game-of-life.py
+++ game-of-life.py
@@ -1,9 +1,11 @@
 import time
 import copy
 import random
+import os
+from sys import platform
 
-generations = 140
-timer = .4
+generations = 1000
+timer = .01
 evolute = .00
 
 
@@ -22,9 +24,18 @@ def generate_board(ymax, xmax):
     return board
 
 game = generate_board(ymax, xmax)
+
+#Presets
+none = []
 pulsar = [[10,7],[10,13],[11,7],[11,13],[12,7],[12,8],[12,12],[12,13],[14,3],[14,4],[14,5],[14,8],[14,9],[14,11],[14,12],[14,15],[14,16],[14,17],[15,5],[15,7],[15,9],[15,11],[15,13],[15,15],[16,7],[16,8],[16,12],[16,13],[18,7],[18,8],[18,12],[18,13],[19,5],[19,7],[19,9],[19,11],[19,13],[19,15],[20,3],[20,4],[20,5],[20,8],[20,9],[20,11],[20,12],[20,15],[20,16],[20,17],[22,7],[22,8],[22,12],[22,13],[23,7],[23,13],[24,7],[24,13]]
-for pos in pulsar:
-    game[pos[0]][pos[1]] = 1
+glider = [[1, 5],[1, 6],[2, 5],[2, 6],[11, 5],[11, 6],[11, 7],[12, 4],[12, 8],[13, 3],[13, 9],[14, 3],[14, 9],[15, 6],[16, 4],[16, 8],[17, 5],[17, 6],[17, 7],[18, 6],[21, 3],[21, 4],[21, 5],[22, 3],[22, 4],[22, 5],[23, 2],[23, 6],[25, 1],[25, 2],[25, 6],[25, 7],[35, 3],[35, 4],[36, 3],[36, 4]]
+#End Presets
+
+
+preset = glider
+
+for pos in preset:
+    game[pos[1]][pos[0]] = 1
 
 """game = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
@@ -128,6 +139,10 @@ for gen in range(generations):
     time.sleep(timer)
     newgame = reset()
     game = update()
+    if platform == 'linux' or platform == 'linux2':
+        os.system('clear')
+    else:
+        os.system('cls')
     print(f"______________gen: {gen+2}____________")
     for y in newgame:
         print(arreplace(y, 0, ' ', 1, "▘"))
