8c81bae1379c1e534ec0e477e99f2f6f36b2969a
Sidharth Rao <sidharthmrao@gmail.com>
2021-05-29 03:20:01 -0400

    Added a random preset. Added functionality to see when a setup becomes stable, along with save-states. Added export capabilities.

--- game-of-life.py
+++ game-of-life.py
@@ -9,10 +9,15 @@ timer = .01
 evolute = .00
 
 
-ymax = 40
+ymax = 50
 xmax = 40
 
 game = []
+
+saveorigin = []
+savestates = []
+
+
 
 def generate_board(ymax, xmax):
     board = []
@@ -27,17 +32,30 @@ game = generate_board(ymax, xmax)
 
 #Presets
 none = []
-pulsar = [[10,7],[10,13],[11,7],[11,13],[12,7],[12,8],[12,12],[12,13],[14,3],[14,4],[14,5],[14,8],[14,9],[14,11],[14,12],[14,15],[14,16],[14,17],[15,5],[15,7],[15,9],[15,11],[15,13],[15,15],[16,7],[16,8],[16,12],[16,13],[18,7],[18,8],[18,12],[18,13],[19,5],[19,7],[19,9],[19,11],[19,13],[19,15],[20,3],[20,4],[20,5],[20,8],[20,9],[20,11],[20,12],[20,15],[20,16],[20,17],[22,7],[22,8],[22,12],[22,13],[23,7],[23,13],[24,7],[24,13]]
-glider = [[1, 5],[1, 6],[2, 5],[2, 6],[11, 5],[11, 6],[11, 7],[12, 4],[12, 8],[13, 3],[13, 9],[14, 3],[14, 9],[15, 6],[16, 4],[16, 8],[17, 5],[17, 6],[17, 7],[18, 6],[21, 3],[21, 4],[21, 5],[22, 3],[22, 4],[22, 5],[23, 2],[23, 6],[25, 1],[25, 2],[25, 6],[25, 7],[35, 3],[35, 4],[36, 3],[36, 4]]
+rand = []
+pulseline = [[20, 6], [20, 7], [20, 8], [20, 9], [20, 10], [20, 11], [20, 12], [20, 13], [20, 14], [20, 15]]
+pulselarg = [[14, 3], [20, 3], [14, 4], [20, 4], [14, 5], [15, 5], [19, 5], [20, 5], [10, 7], [11, 7], [12, 7], [15, 7], [16, 7], [18, 7], [19, 7], [22, 7], [23, 7], [24, 7], [12, 8], [14, 8], [16, 8], [18, 8], [20, 8], [22, 8], [14, 9], [15, 9], [19, 9], [20, 9], [14, 11], [15, 11], [19, 11], [20, 11], [12, 12], [14, 12], [16, 12], [18, 12], [20, 12], [22, 12], [10, 13], [11, 13], [12, 13], [15, 13], [16, 13], [18, 13], [19, 13], [22, 13], [23, 13], [24, 13], [14, 15], [15, 15], [19, 15], [20, 15], [14, 16], [20, 16], [14, 17], [20, 17]]
+glidergun = [[25, 1], [23, 2], [25, 2], [13, 3], [14, 3], [21, 3], [22, 3], [35, 3], [36, 3], [12, 4], [16, 4], [21, 4], [22, 4], [35, 4], [36, 4], [1, 5], [2, 5], [11, 5], [17, 5], [21, 5], [22, 5], [1, 6], [2, 6], [11, 6], [15, 6], [17, 6], [18, 6], [23, 6], [25, 6], [11, 7], [17, 7], [25, 7], [12, 8], [16, 8], [13, 9], [14, 9]]
 #End Presets
 
 
-preset = glider
+preset = pulseline
 
-for pos in preset:
-    game[pos[1]][pos[0]] = 1
+if preset == "rand":
+    points = random.randint(0, ymax*xmax)
+    for point in range(points):
+        rand.append([])
+    for point in range(len(rand)):
+        rand[point] = [random.randint(0, xmax-1), random.randint(0, ymax-1)]
+    for pos in rand:
+        game[pos[1]][pos[0]] = 1
+else:
+    for pos in preset:
+        game[pos[1]][pos[0]] = 1
 
-"""game = [
+
+
+game = """[
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
@@ -116,6 +134,9 @@ def update():
                 newgame[y][x] = 1
             if evolve >= 1-evolute:
                 newgame[y][x] = 0
+    
+    savestates.append(newgame)
+
     return newgame
 
 def arreplace(gamearray, target, replaced, target2, replaced2):
@@ -129,21 +150,45 @@ def arreplace(gamearray, target, replaced, target2, replaced2):
     return arr
 
 
-
+saveorigin = copy.deepcopy(game)
 
 print(f"______________gen: original_______")
 for y in game:
     print(arreplace(y, 0, ' ', 1, "▘"))
 
-for gen in range(generations):
-    time.sleep(timer)
-    newgame = reset()
-    game = update()
-    if platform == 'linux' or platform == 'linux2':
-        os.system('clear')
-    else:
-        os.system('cls')
-    print(f"______________gen: {gen+2}____________")
-    for y in newgame:
-        print(arreplace(y, 0, ' ', 1, "▘"))
+class releaseMe(Exception):
+    pass
+
+try:
+    for gen in range(generations):
+        time.sleep(timer)
+        newgame = reset()
+        game = update()
+        if platform == 'linux' or platform == 'linux2':
+            os.system('clear')
+        else:
+            os.system('cls')
+        print(f"______________gen: {gen+2}____________")
+        for y in newgame:
+            print(arreplace(y, 0, ' ', 1, "▘"))
+        
+        for save in savestates[:-1]:
+            if save == newgame:
+                print(f"Provably stable or empty after {gen+2} generations.")
+                for y in saveorigin:
+                    print(arreplace(y, 0, ' ', 1, "▘"))
+                raise releaseMe
+except releaseMe:
+    pass
+
+def export():
+    exported = []
+    for y in ren(saveorigin):
+        for x in ren(saveorigin[y]):
+            if saveorigin[y][x] == 1:
+                exported.append([x,y])
     
+    return exported
+
+print(export())
+        
